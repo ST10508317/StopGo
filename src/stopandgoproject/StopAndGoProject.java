bce12db4b63b76590e1c909b91c657adf8551591
ST10508317 <zaheercamrodien06@gmail.com>
2026-02-25 08:17:12 +0200

    Your Commit Message

---
+++ src/stopandgoproject/StopAndGoProject.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stopandgoproject;
+
+/**
+ *
+ * @author ST10508317
+ * @author ZaheerCamrodien
+ */
+public class StopAndGoProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        //This code is used to display "Stop" and "Go" signs.
+        
+    System.out.println("   ********       ********    ");  
+    System.out.println(" *          *   *          * ");     
+    System.out.println("*    STOP    * *     GO     *  "); 
+    System.out.println(" *          *   *          *"); 
+    System.out.println("   ********       ********     ");         
+    System.out.println("      **             **        ");         
+    System.out.println("      **             **       ");        
+    System.out.println("      **             **");        
+    System.out.println("      **             **        ");        
+    System.out.println("     ****            **         "); 
+    System.out.println("   ********        ********  "); 
+                      
+   }
+    
+}
